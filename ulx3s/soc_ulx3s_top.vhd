318a1c7f247b097954e9408f93c4a86a4b030a46
Thomas Hornschuh <thomas@bonfirecpu.eu>
2020-11-04 01:36:42 +0100

    SPI to NAND Flash added for ULX3S

--- ulx3s/soc_ulx3s_top.vhd
+++ ulx3s/soc_ulx3s_top.vhd
@@ -31,6 +31,14 @@ entity soc_ulx3s_top is
           uart0_txd : out std_logic;
           uart0_rxd : in  std_logic :='1';
 
+          -- SPI NOR Flash
+          flash_csn   : out std_logic;
+          --flash_clk  : out std_logic;
+          flash_mosi : out std_logic;
+          flash_miso : in  std_logic;
+          flash_holdn :  out std_logic;
+          flash_wpn : out std_logic;
+
           led : out std_logic_vector(7 downto 0)
     );
 
@@ -79,16 +87,26 @@ architecture Behavioral of soc_ulx3s_top is
     );
  end component bonfire_basic_soc_top;
  
- component clock_gen
-    port (CLKI: in  std_logic; CLKOP: out  std_logic);
-end component;
+	component clock_gen
+		port (CLKI: in  std_logic; CLKOP: out  std_logic);
+	end component;
 
-
+	COMPONENT USRMCLK
+	PORT(
+		USRMCLKI : IN STD_ULOGIC;
+		USRMCLKTS : IN STD_ULOGIC
+	);
+	END COMPONENT;
+	attribute syn_noprune: boolean ;
+	attribute syn_noprune of USRMCLK: component is true;
 
 
 signal reset,res1,res2  : std_logic;
 signal clk_locked : std_logic;
 signal clk : std_logic;
+
+signal flash_clk : std_logic;
+signal flash_csn_local : std_logic;
 
 signal gpio_o         : std_logic_vector(LED'range);
 signal gpio_i         : std_logic_vector(LED'range);
@@ -97,8 +115,16 @@ signal gpio_t         : std_logic_vector(LED'range);
 
 begin
 
+  flash_holdn <= '1';
+  flash_wpn <= '1';
+  flash_csn <= flash_csn_local;
 
   pll : clock_gen port map (CLKI=>sysclk, CLKOP=>clk);
+  
+  -- See Lattice TN1260 Figure 7: MCLK Connection 
+  u1: USRMCLK port map (
+		USRMCLKI => flash_clk,
+		USRMCLKTS => flash_csn_local); -- CS active low will also disable Tristate status
 
 
   bonfire_basic_soc_top_i :  bonfire_basic_soc_top
@@ -111,7 +137,7 @@ begin
         Swapbytes        => false,
         -- ExtRAM           => ExtRAM,
         ENABLE_UART1     => false,
-        ENABLE_SPI       => false,
+        ENABLE_SPI       => true,
         USE_BONFIRE_CORE => false,
         -- BurstSize        => BurstSize,
         -- CacheSizeWords   => CacheSizeWords,
@@ -130,10 +156,10 @@ begin
         uart0_rxd      => uart0_rxd,
         uart1_txd      => open,
         uart1_rxd      => '1',
-        flash_spi_cs   => open,
-        flash_spi_clk  => open,
-        flash_spi_mosi => open,
-        flash_spi_miso => '0',
+        flash_spi_cs   => flash_csn_local,
+        flash_spi_clk  => flash_clk,
+        flash_spi_mosi => flash_mosi,
+        flash_spi_miso => flash_miso,
         gpio_o => gpio_o,
         gpio_i => gpio_i,
         gpio_t => gpio_t
